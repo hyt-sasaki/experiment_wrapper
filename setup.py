c2e6043f2d77974c2406ce6f5060da08190733b0
Sasaki Hayato <h.sasaki.ynu@gmail.com>
2016-02-09 01:49:58 +0900

    [change] ディレクトリの構成を変更し, setup.pyを追加
    setup.pyを用いてスクリプトをインストールできるように変更

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='exp_wrapper',
+    version='1.0',
+    author='Hayato Sasaki',
+    packages=['exp_wrapper'],
+    install_requires=['GitPython'],
+    scripts=['src/experiment'],
+    package_dir={'': 'src'}
+)
