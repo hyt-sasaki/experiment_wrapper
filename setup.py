2f463378c4f9ff2b3694cfc29b2d1d22b0c959dd
Hayato Sasaki <h.sasaki.ynu@gmail.com>
2017-06-11 02:47:58 +0900

    [upgrade] v1.0 -> v1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='exp_wrapper',
-    version='1.0',
+    version='1.1',
     author='Hayato Sasaki',
     packages=['exp_wrapper'],
     install_requires=['GitPython'],
